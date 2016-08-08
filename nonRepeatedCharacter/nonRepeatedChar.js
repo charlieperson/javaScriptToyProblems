5b3108bb35136333bb7db94a16108de5735a29d4
charlieperson <charlie.person@hyperisland.se>
2016-08-07 19:01:49 -0500

    Completes first non repeated character problem

--- nonRepeatedCharacter/nonRepeatedChar.js
+++ nonRepeatedCharacter/nonRepeatedChar.js
@@ -1,3 +1,11 @@
 function firstNonRepeatedCharacter (string) {
-
+  for(var i=0; i<string.length; i++) {
+    var currentLetter = string[i];
+    var count = 0;
+    for(var j=0; j<string.length; j++) {
+      if(string[j] === currentLetter) count += 1;
+    }
+    if(count < 2) return currentLetter;
+  }
+  return 'sorry';
 }
