2e3286f7143b021edfd031d994808c385cd73aa9
charlieperson <charlie.person@hyperisland.se>
2016-10-17 22:19:45 -0500

    Completes arrayZipSum problem

--- arrayZipSum/arrayZipSum.js
+++ arrayZipSum/arrayZipSum.js
@@ -1,4 +1,7 @@
 function zipSum (xs, ys) {
-  // Write your code here, and
-  // return your final answer.
+  var smallest;
+  xs.length < ys.length ? smallest = xs : smallest = ys;
+  return smallest.map((item, i) => {
+    return xs[i] + ys[i];
+  })
 }
