edbdf35a7c1864b4a0c9544249d4fbbd4930afa3
charlieperson <charlie.person@hyperisland.se>
2016-10-18 20:15:46 -0500

    Partially completes longest improvement

--- longestImprovement/longestImprovement.js
+++ longestImprovement/longestImprovement.js
@@ -1,4 +1,15 @@
 function longestImprovement (numbers) {
-  // Write your code here, and
-  // return your final answer.
+  function longestImprovement (numbers) {
+    var streak = 0;
+    var gettingThere = 0;
+    numbers.forEach((num, i) => {
+      if(num < numbers[i+1]) {
+        gettingThere += 1
+        if(gettingThere > streak) streak = gettingThere;
+      } else {
+        gettingThere = 0
+      }
+    })
+    return streak;
+  }
 }
