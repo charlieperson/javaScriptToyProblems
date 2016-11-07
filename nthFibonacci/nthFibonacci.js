38635e9b73d74efeadfe8c2e7973e62cb611564b
charlieperson <charlie.person@hyperisland.se>
2016-11-06 21:28:46 -0600

    Adds nth fibonacci problem with README fully complete

---
+++ nthFibonacci/nthFibonacci.js
@@ -0,0 +1,10 @@
+nthFibonacci = function(n) {
+    if(n === 0 || n===1) return n
+    var a = 0, b = 1, f = 1;
+    for(var i = 2; i <= n; i++) {
+        f = a + b;
+        a = b;
+        b = f;
+    }
+    return f;
+};
