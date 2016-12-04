1c4ff182db7ec160ce94c2840f24ff755ca8c176
charlieperson <charlie.person@hyperisland.se>
2016-12-03 19:16:31 -0600

    Successfully checking rows for sudokuChecker- not yet checking columns/boxes

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -1,4 +1,15 @@
 function sudokuCheck (boardStr) {
-  // Write your code here, and
-  // return your final answer.
+  var solved = true;
+  var rows = boardStr.split("\n")
+  console.log('ROWS: ', rows)
+  for(let i=0; i<rows.length; i++) {
+    var currentRow = rows[i].split('')
+    var numberStorage = []
+    currentRow.forEach((digitString) => {
+      numberStorage.push(parseInt(digitString))
+    })
+    if(numberStorage.sort().join('') !== '123456789') solved = false
+  }
+
+  return solved
 }
